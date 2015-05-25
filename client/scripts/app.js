0a14ce59a18decd780ac4cba7fd9a0ebbacd8260
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 16:32:05 -0700

    implement GET, show, refresh, post, and sanitize messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,32 +1,7 @@
 //https://api.parse.com/1/classes/chatterbox
+var app = {};
 
-(function () {
-  'use strict';
-
-  var message = {
-    'username': '<script type="text/javascript"> alert("escape yoself befo you wreck yoself");</script>',
-    'text': '<script type="text/javascript"> alert("escape yoself befo you wreck yoself");</script>',
-    'roomname': 'test'
-  };
-
-  //POST message to Parse server
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-
-  var recieved = {};
-
+app.init = function() {
   //GET messages from Parse server
   $.ajax({
     // always use this url
@@ -34,20 +9,71 @@
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      //debugger;
-      console.log(data.results);
-      console.log('chatterbox: Got messages!');
-      recieved = data.results;
-      _.each(data.results, function(m) {
-        console.log(m.username)
-      })
+      app.populateMessages(data.results);
+      console.log('Chatterbox: Got messages');
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to get message');
+      console.error('Chatterbox: Failed to get messages');
     }
   });
-  console.log('recieved: ')
-  console.dir(recieved);
+};
 
-})();
+app.sanitize = function(string) {
+  var string = string || "";
+  return string.replace(/<.*?>/, "");
+}
+
+app.populateMessages = function(messages) {
+  //append a message div to message-container div
+  _.each(messages, function(message) {
+    var text = app.sanitize(message.text);
+    var username = app.sanitize(message.username);
+    var date = app.sanitize(message.createdAt);
+    var roomname = app.sanitize(message.roomname);
+
+    var $message_html =
+    "<div class='message' room=" + roomname + ">" +
+      "<p class='message-text'>" + text + "</p>" +
+      "<p class='message-username'>" + username + "</p>" +
+      "<p class='message-date'>" + date + "</p>" +
+      "<p class='message-room'>" + roomname + "</p></div>"
+   $('.message-container').append($message_html);
+  });
+
+};
+
+app.init();
+
+var message = {
+  'username': '<img src="http://i.imgur.com/cu0UCYu.gif">',
+  'text': 'HAPPY PUG TIME',//'<script type="text/javascript"> $(\'body\').css(\'background\', \'url("http://38.media.tumblr.com/tumblr_meazvxSXKx1qkrm4go1_400.gif")\');</script>',
+  'roomname': 'PUGTIME'
+};
+
+//POST message to Parse server
+
+  // $.ajax({
+  //   // always use this url
+  //   url: 'https://api.parse.com/1/classes/chatterbox',
+  //   type: 'POST',
+  //   data: JSON.stringify(message),
+  //   contentType: 'application/json',
+  //   success: function (data) {
+  //     console.log('chatterbox: Message sent');
+  //   },
+  //   error: function (data) {
+  //     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //     console.error('chatterbox: Failed to send message');
+  //   }
+  // });
+  // console.log(i);
+  // i++;
+
+
+
+$(document).ready(function(){
+  $(".refresh-button").click(function(event) {
+    app.init();
+  });
+});
