16d20b67cf9214c056c0f8a355eef8e2949cc4e0
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 17:17:33 -0700

    add message posting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 //https://api.parse.com/1/classes/chatterbox
 var app = {};
+app.lastUpdate;
 
 app.init = function() {
   //GET messages from Parse server
@@ -26,10 +27,11 @@ app.sanitize = function(string) {
 
 app.populateMessages = function(messages) {
   //append a message div to message-container div
+  app.lastUpdate = messages[0].createdAt;
   _.each(messages, function(message) {
     var text = app.sanitize(message.text);
     var username = app.sanitize(message.username);
-    var date = app.sanitize(message.createdAt);
+    var date = moment(app.sanitize(message.createdAt)).fromNow();
     var roomname = app.sanitize(message.roomname);
 
     var $message_html =
@@ -38,42 +40,53 @@ app.populateMessages = function(messages) {
       "<p class='message-username'>" + username + "</p>" +
       "<p class='message-date'>" + date + "</p>" +
       "<p class='message-room'>" + roomname + "</p></div>"
-   $('.message-container').append($message_html);
+    $('.message-container').append($message_html);
   });
 
 };
 
 app.init();
 
-var message = {
-  'username': '<img src="http://i.imgur.com/cu0UCYu.gif">',
-  'text': 'HAPPY PUG TIME',//'<script type="text/javascript"> $(\'body\').css(\'background\', \'url("http://38.media.tumblr.com/tumblr_meazvxSXKx1qkrm4go1_400.gif")\');</script>',
-  'roomname': 'PUGTIME'
-};
+
 
 //POST message to Parse server
 
-  // $.ajax({
-  //   // always use this url
-  //   url: 'https://api.parse.com/1/classes/chatterbox',
-  //   type: 'POST',
-  //   data: JSON.stringify(message),
-  //   contentType: 'application/json',
-  //   success: function (data) {
-  //     console.log('chatterbox: Message sent');
-  //   },
-  //   error: function (data) {
-  //     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //     console.error('chatterbox: Failed to send message');
-  //   }
-  // });
-  // console.log(i);
-  // i++;
 
+
+
+app.submitMessage = function(username, text, roomname) {
+  var message = {
+    'username': username,
+    'text': text,
+    'roomname': roomname
+  };
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
 
 
 $(document).ready(function(){
-  $(".refresh-button").click(function(event) {
+  $(".refresh-button").click(function() {
     app.init();
+  });
+
+  $('.message-form').submit(function(e) {
+    e.preventDefault();
+
+    var username = $("[name='username']").val();
+    var text = $("[name='text']").val();
+    var roomname = $("[name='roomname']").val();
+    app.submitMessage(username, text, roomname);
   });
 });
